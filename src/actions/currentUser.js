d954d0d230e2775b390b4498265ef88cc00946f4
Cl3arglass <haltaffer@gmail.com>
2019-07-11 15:55:38 -0700

    refactor to use dispatch instead of setState

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -8,14 +8,53 @@ export const setCurrentUser = user => {
 
 //async
 export const login = credentials => {
-	return {
        return dispatch => {
        	return fetch("http://localhost:3001/api/login", {
        		method: "POST",
        		headers: {
        			"Content-Type": "application/json"
        		},
-       		body: JSON.stringify({email: "lass@lass.com", password: "password"})
+       		body: JSON.stringify(credentials)
        })
+       	.then(result => result.json())
+       	.then(userJSON => {
+         if (userJSON.error) {
+            alert(userJSON.error)
+         } else {
+            dispatch(setCurrentUser(userJSON))
+            localStorage.setItem('token', userJSON.jwt)
+         }
+       })
+       .catch(console.log)
 	}
+	// event.preventDefault()
+
+ //    const userInfo = this.state.loginForm
+ //    const headers = {
+ //      method: "POST",
+ //      headers: {
+ //        "Content-Type": "application/json"
+ //      },
+ //      body: JSON.stringify({
+ //        user: userInfo 
+ //      })
+ //    }
+
+ //    fetch("http://localhost:3001/api/login", headers)
+ //       .then(result => result.json())
+ //       .then(userJSON => {
+ //         if (userJSON.error) {
+ //            alert(userJSON.error)
+ //         } else {
+ //            this.setState({
+ //              currentUser: userJSON.user, 
+ //              loginForm: {
+ //                email: "",
+ //                password: ""
+ //              }
+ //            })
+ //            localStorage.setItem('token', userJSON.jwt)
+ //         }
+ //       })
+ //       .catch(console.log)
 }
