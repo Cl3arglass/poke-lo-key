68fe6997200f33ca1ba83312f66a1fcbb753d35b
Cl3arglass <haltaffer@gmail.com>
2019-07-29 20:03:21 -0700

    add pokes actions set and get pokes

---
+++ src/actions/pokes.js
@@ -0,0 +1,48 @@
+//synchronous
+export const setPokes = pokes => {
+	return {
+		type: "SET_POKES",
+		pokes: pokes
+	}
+}
+
+//async
+export const getPokes = () => {
+	const token = localStorage.getItem("token")
+       return dispatch => {
+       	return fetch("http://localhost:3001/api/pokes", {
+       		method: "GET",
+       		headers: {
+       			"Authorization": token
+       		},
+       })
+       	.then(result => result.json())
+       	.then(pokesJSON => {
+         if (pokesJSON.error) {
+            alert(pokesJSON.error)
+         } else {
+            dispatch(setPokes(pokesJSON.data))
+         }
+       })
+       .catch(console.log)
+	}
+}
+// getPokes = () => {
+  //   const token = localStorage.getItem("token")
+  //   fetch("http://localhost:3001/api/pokes", {
+  //     headers: {
+  //       "Authorization": token
+  //     }
+  //   })
+  //     .then(result => result.json())
+  //     .then(pokes => {
+  //       if (pokes.error) {
+  //          alert("Not authorized for those secrets")
+  //       } else {
+  //         this.setState({
+  //           pokes
+  //         })
+  //       }
+  //     })
+  //     .catch(console.log)
+  // }
