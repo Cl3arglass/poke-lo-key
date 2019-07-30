0ece3ef6d1a2aa654d06080f62cbb35807d0541f
Cl3arglass <haltaffer@gmail.com>
2019-07-29 20:12:18 -0700

    add and connect mapstatetoprops and import and return pokecard

--- src/components/Pokes.js
+++ src/components/Pokes.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PokeCard from './PokeCard.js'
 
 const Pokes = ({ pokes }) => {
-   const pokesJSX = pokes.map(p => <p key={p.id}>{p.name}</p>)
+   // const pokesJSX = pokes.map(p => <p key={p.id}>{p.name}</p>)
+   const pokesCards = pokes.map(p => <PokeCard poke={p} key={p.id}/>)
    return (
    	<div className="Pokes">
-   	  { pokesJSX }
+   	  { pokesCards }
    	</div>
    	)
 }
 
-export default Pokes
+
+
+const mapStateToProps = state => {
+   return {
+      pokes: state.pokes
+   }
+}
+export default connect(mapStateToProps)(Pokes)
