10b62600f6ea5df4cc0ae0f6a18db9ca6036b311
Cl3arglass <haltaffer@gmail.com>
2019-08-06 11:15:01 -0700

    refactor navbar and include signup form  on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Login from './Login'
 import Logout from './Logout'
+import Signup from './Signup'
 import { connect } from 'react-redux'
 
 const Navbar = ({ currentUser }) => {
-	return (
-       <div>
-          { currentUser ? `Welcome, ${currentUser.attributes.name}` : ""}
-          { currentUser ? <Logout/> : <Login/> }
-       </div>
-    )
+	if (currentUser) {
+		return (
+			<div>
+				Welcome, {currentUser.attributes.name}
+				<Logout/>
+			</div>
+			)
+	} else {
+		return (
+		<div>
+			Please signup or login
+			<Signup/>
+			<Login/>
+		</div>
+	    )
+	}
+// return (
+ //       <div>
+ //          { currentUser ? `Welcome, ${currentUser.attributes.name}` : ""}
+ //          { currentUser ? <Logout/> : <Login/> }
+ //       </div>
+ //    )
 }
 
 const mapStateToProps = ({ currentUser }) => {
