89c12bee3ba9380618621940c87282f82e69df3f
Cl3arglass <haltaffer@gmail.com>
2019-08-07 19:25:10 -0700

    add new poke form reducer with update, reset and an initial state

---
+++ src/reducers/newPokeForm.js
@@ -0,0 +1,17 @@
+const initialState = {
+	name: "",
+	location: "", 
+	found: ""
+}
+
+
+export default (state=initialState, action) => {
+	switch (action.type) {
+		case "UPDATE_NEW_POKE_FORM":
+		  return action.formData
+		case "RESET_NEW_POKE_FORM":
+		  return initialState
+		default:
+		  return state 
+	}
+}
