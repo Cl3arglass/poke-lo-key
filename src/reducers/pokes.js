7d7b9be4120471f63e78a40d3caac2727973446a
Cl3arglass <haltaffer@gmail.com>
2019-08-11 11:46:47 -0700

    add clear pokes action to return initial state

--- src/reducers/pokes.js
+++ src/reducers/pokes.js
@@ -1,7 +1,11 @@
-export default (state = [], action) => {
+const initialState = []
+
+export default (state = initialState, action) => {
  	switch (action.type) {
  		case "SET_POKES":
  		  return action.pokes
+ 		 case "CLEAR_POKES":
+ 		  return initialState
  		default:
  		  return state
  	}
