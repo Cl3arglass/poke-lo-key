ebf08b9e9e50a2520d677ef248ecc49a883dd20c
Cl3arglass <haltaffer@gmail.com>
2019-08-14 15:00:00 -0700

    add add poke to reducer and return pokes plus new

--- src/reducers/pokes.js
+++ src/reducers/pokes.js
@@ -4,6 +4,8 @@ export default (state = initialState, action) => {
  	switch (action.type) {
  		case "SET_POKES":
  		  return action.pokes
+ 		case "ADD_POKE":
+ 		  return state.pokes.concat(action.trips)
  		 case "CLEAR_POKES":
  		  return initialState
  		default:
