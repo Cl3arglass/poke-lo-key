f03c20f903819a6e08dc07ea241363257cb16582
Cl3arglass <haltaffer@gmail.com>
2019-08-15 16:34:16 -0700

    refactor pokecard to sow location and found as well

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -4,7 +4,13 @@ import React from 'react'
 const PokeCard = ({ poke }) => {
    
    return (
-   	<p>{poke.attributes.name}</p>
+   	poke ?
+   	<div>
+	   	<h3>{poke.attributes.name}</h3>
+	   	<p>Location: {poke.attributes.location}</p>
+	   	<p>Found? {poke.attributes.found}</p>
+   	</div> :
+   	<p> Pokes go here</p>
    	)
 }
 
