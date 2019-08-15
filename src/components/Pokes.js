6de26ec9314966d1690752661948190d75c309a8
Cl3arglass <haltaffer@gmail.com>
2019-08-15 16:35:16 -0700

    add sow links to poke sow instead of pokecards

--- src/components/Pokes.js
+++ src/components/Pokes.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import PokeCard from './PokeCard.js'
+import { Link } from 'react-router-dom'
 
 const Pokes = ({ pokes }) => {
    // const pokesJSX = pokes.map(p => <p key={p.id}>{p.name}</p>)
-   const pokesCards = pokes.map(p => <PokeCard poke={p} key={p.id}/>)
+   const pokesLinks = pokes.map(p => (<><Link  key={p.id} to={`/pokes/${p.id}`}>{p.attributes.name}</Link><br/></>))
    return (
    	<div className="Pokes">
-   	  { pokesCards }
+   	  { pokesLinks }
    	</div>
    	)
 }
