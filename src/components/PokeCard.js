f7e0fad5db8ad9482948a70d67b5b315e3a31c04
Cl3arglass <haltaffer@gmail.com>
2019-08-16 11:26:50 -0700

    add edit link to poke card

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const PokeCard = ({ poke }) => {
    
@@ -9,9 +9,11 @@ const PokeCard = ({ poke }) => {
 	   	<h3>{poke.attributes.name}</h3>
 	   	<p>Location: {poke.attributes.location}</p>
 	   	<p>Found? {poke.attributes.found}</p>
+        <Link to={`/pokes/${poke.id}/edit`}>Edit Poke</Link>
    	</div> :
    	<p> Pokes go here</p>
    	)
 }
 
 export default PokeCard
+// props.match.path === "/pokes/:id/edit"
