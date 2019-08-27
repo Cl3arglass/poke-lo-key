0aaf1bc67a9fa9994890bdb9a0abf88a52bac2b2
Cl3arglass <haltaffer@gmail.com>
2019-08-27 12:45:49 -0700

    refactor home not using props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Login from './Login'
-import Logout from './Logout'
-import Signup from './Signup'
 import { Link } from 'react-router-dom'
 
-const Home = ({}) => (
+const Home = () => (
 	<div>
 	  <h4>Welcome, please <Link to="/signup">Sign Up</Link> or <Link to="/login">Login</Link></h4>
 	</div>
