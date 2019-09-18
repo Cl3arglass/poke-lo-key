09f31595b36efb1a113ccf06f9e2cfc290180cee
Cl3arglass <haltaffer@gmail.com>
2019-09-17 17:17:28 -0700

    add id to each poke for react

--- src/components/Pokes.js
+++ src/components/Pokes.js
@@ -5,7 +5,7 @@ import { likePoke } from "../actions/pokes"
 
 const Pokes = ({ pokes, likePoke }) => {
    // const pokesJSX = pokes.map(p => <p key={p.id}>{p.name}</p>)
-   const pokesLinks = pokes.map(p => (<div class="wrapper"><Link  key={p.id} to={`/pokes/${p.id}`}>{p.attributes.name}</Link>
+   const pokesLinks = pokes.map(p => (<div key={p.id} className="wrapper"><Link to={`/pokes/${p.id}`}>{p.attributes.name}</Link>
       <p>Likes: {p.attributes.likes}</p> <button onClick={()=>likePoke(p)}>Like Poke</button><br/></div>))
       
       
