cb07e0d8d9d5aff466807efc319a980be98b319b
Cl3arglass <haltaffer@gmail.com>
2019-09-26 13:59:59 -0700

    add pokeSearch component

---
+++ src/components/PokeSearch.js
@@ -0,0 +1,14 @@
+// import 'react' from React
+// import { connect } from 'react-redux'
+// import { searchPoke } from "../actions/pokes"
+
+// const PokeSearch = ({ searchPoke }) => {
+// 	return (
+// 		<div>   
+// 	   	  <button onClick={()=>searchPoke()}>Search Poke</button>
+// 	   	</div>
+// 		)
+// }
+
+
+// export default connect(null, {searchPoke})(PokeSearch)
